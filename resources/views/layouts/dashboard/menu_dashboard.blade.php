a07e7f772732c184870657599b2ba58c34782e00
LouckGuntz <emanuelsaucedo52@gmail.com>
2022-12-05 20:49:48 -0600

    0.1.16 - correciones y dominio

--- resources/views/layouts/dashboard/menu_dashboard.blade.php
+++ resources/views/layouts/dashboard/menu_dashboard.blade.php
@@ -4,7 +4,7 @@
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ url('/') }}">
         <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
+            <img src="{{ asset('img/snapup1.png') }}"  alt="">
         </div>
         <div class="sidebar-brand-text mx-3">SnapUp</div>
     </a>
